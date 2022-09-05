2d051fe9d2e3c36778def4020dc8692c34e5d20b
MarwanMohamed02 <marwanabbas2002@yahoo.com>
2022-09-06 00:11:17 +0200

    deletes

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ try {
 catch (err) {
     io.emit("db_error");
 }
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // const publicDir = path.join(__dirname, '../../public');
 const clientDir = path_1.default.join(__dirname, "../../dist/public");
 let user;
